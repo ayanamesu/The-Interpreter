a6f2b25469924e4276bc89e4fa36f0ea43cd96a2
ayanamesu <leejacky74@gmail.com>
2023-07-01 20:27:46 -0700

    first commit

--- interpreter/bytecodes/HaltCode.java
+++ interpreter/bytecodes/HaltCode.java
@@ -1,7 +1,12 @@
 package interpreter.bytecodes;
-
+import interpreter.virtualmachine.VirtualMachine;
 public class HaltCode implements ByteCode{
     public HaltCode(String[] args) {
 
     }
+    @Override
+    public void execute(VirtualMachine vm) {
+        vm.halt(); // Request the VirtualMachine to halt execution
+    }
 }
+
